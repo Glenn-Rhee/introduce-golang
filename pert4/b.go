c732d7df7dd2e26aa15a6ba6d666b4c16a8aa220
Glenn-Rhee <glennviktor5@gmail.com>
2025-05-07 18:42:15 +0700

    fix: fix code

--- pert4/b.go
+++ pert4/b.go
@@ -2,15 +2,16 @@ package main
 import "fmt"
 
 func main(){
-	var buah_toko_ariel = [5]string{"Apel", "Semangka", "Jeruk", "Anggur", "Pepaya"}
-	var toko_ariel = buah_toko_ariel[0:5]
-	var buah_bulan_ini = [8]string{"Semangka", "Jeruk", "Anggur", "Rambutan", "Mangga", "Kelengkeng"}
-	var katalog_bulanan = buah_bulan_ini[0:6]
+	tokoAriel := []string{"Apel", "Semangka", "Jeruk", "Anggur", "Pepaya"}
+	fmt.Println("Isi Toko Buah Ariel yaitu: ", tokoAriel)
 
-	fmt.Println("Isi Toko Buah Ariel yaitu\t: ", toko_ariel)
-	fmt.Println("Katalog buah bulan ini yaitu\t: ", katalog_bulanan)
-	fmt.Println("Panjang Katalog Buah\t\t: ", len(toko_ariel))
-	fmt.Println("Kapasitas Katalog Buah\t\t:", cap(buah_toko_ariel))
-	fmt.Println("Panjang Katalog Buah Bulanan\t:", len(katalog_bulanan))
-	fmt.Println("Kapasistas Katalog Buah Bulanan\t:", cap(katalog_bulanan))
+	var katalogBulanan = tokoAriel[1:4]
+	katalogBulanan = append(katalogBulanan, "Rambutan", "Mangga", "Kelengkeng")
+	fmt.Println("Katalog buah bulan ini yaitu: ", katalogBulanan)
+
+	fmt.Println("Panjang katalog Buah: ", len(tokoAriel))
+	fmt.Println("Kapasitas katalog buah: ", cap(tokoAriel))
+	fmt.Println("Panjang katalog buah bulanan: ", len(katalogBulanan))
+	fmt.Println("kapasitas katalog buah bulanan: ", cap(katalogBulanan))
+	
 }
